6456937442cfb4dc09d31a6aa0ed96c4c9d54253
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-11-27 21:59:09 +0530

    2 done

--- sleep_efficiency_db.sql
+++ sleep_efficiency_db.sql
@@ -1,4 +1,4 @@
--- Find out the average sleep duration of top 15 male candidates who's sleep duration are equal to 7.5 or greater than 7.5.
+-- Find out the average sleep duration of top 15 male candidates whose sleep duration are equal to 7.5 or greater than 7.5.
 select se.ID,avg(se.`Sleep duration`) as 'avg_sleep'
 from se
 where `Sleep duration`>=7.5
@@ -7,3 +7,11 @@ order by avg_sleep desc
 limit 15
 
 
+--  Show avg deep sleep time for both gender. Round result at 2 decimal places.
+select Gender,round(avg(`Deep sleep percentage`),2) as 'avg_deep_sleep'
+from se
+group by Gender
+
+
+
+--
